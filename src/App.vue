38dfb766ff9cf2233862052cc003d3c004a15e91
ryan <laslomcd@gmail.com>
2020-12-22 17:07:48 -0500

    working on search box

--- src/App.vue
+++ src/App.vue
@@ -33,18 +33,17 @@
 		</header>
 		<section class="flex justify-between bg-gray-800 px-4 py-3">
 			<div class="relative">
-				<div class="absolute inset-y-0 left-0">
-					<svg xmlns="http://www.w3.org/2000/svg" width="12" height="13">
+				<div class="flex items-center absolute inset-y-0 left-0 pl-3">
+					<svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3 fill-current text-gray-500">
 						<g stroke-width="2" stroke="#6c6c6c" fill="none">
 							<path d="M11.29 11.71l-4-4" />
 							<circle cx="5" cy="5" r="4" />
 						</g>
 					</svg>
 				</div>
-
-				<input class="bg-gray-900 text-white rounded px-4 py-3" placeholder="Search by keywords" />
-				<button>Filters</button>
+				<input class="bg-gray-900 text-white rounded px-8 py-3" placeholder="Search by keywords" />
 			</div>
+			<button>Filters</button>
 		</section>
 		<main>
 			<h3>Los Angeles</h3>
